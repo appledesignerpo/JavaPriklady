dd057b2a73966c30c881b289eced23bb83da0c48
pavere <kostrab@icloud.com>
2023-08-27 21:06:18 +0200

    test finnancnej aplikacie

--- src/ulohy/PriradenieZnamkyTextovemuHodnoteniu.java
+++ src/ulohy/PriradenieZnamkyTextovemuHodnoteniu.java
@@ -37,7 +37,7 @@ public class PriradenieZnamkyTextovemuHodnoteniu {
         switch (vstupnaTextovaZnamka) {
             case ("vyborny") -> {
                 System.out.println(1);
-                System.out.println("Si najlepší");
+                System.out.println("Si najlepší T");
             }
 
             case ("chvalitebny") ->
