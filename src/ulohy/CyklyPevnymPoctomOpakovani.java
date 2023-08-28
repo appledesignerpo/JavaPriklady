8edefadc8d349e4545e5e7a41bc5734e90d7574b
pavere <kostrab@icloud.com>
2023-08-28 19:17:20 +0200

    polia

--- src/ulohy/CyklyPevnymPoctomOpakovani.java
+++ src/ulohy/CyklyPevnymPoctomOpakovani.java
@@ -25,6 +25,7 @@ public class CyklyPevnymPoctomOpakovani {
         }
 
         System.out.println();//prazdny riadok
+
         //vnaranie cyklov
         //TODO ako vypista hodnoty od 1 -100 aby v prvom vypsial od 1 do 10 v druhom od 11 do 20 v tretom od 21 -do 30 atd
         for (int i = 1; i <=100 ; i++) {
@@ -33,6 +34,40 @@ public class CyklyPevnymPoctomOpakovani {
 
 
         }
+
+        System.out.println("\nPodobná úloha ako vyššie s využitim 2 cyklov, resp. vnoreného");
+        // Podobna uloha ako vyssie s vyuzitim 2 cyklov, resp. vnoreneho
+        for (int i = 0; i <= 9; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.print((10 * i + j) + "\t");
+            }
+            System.out.println();
+        }
+
+        // matic s udajmiu 1  do 25
+        int rows = 5;
+        int cols = 5;
+        int[][] matrix = new int[rows][cols];
+
+        int value = 1;
+
+        // Naplnění matice hodnotami od 1 do 25
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = value;
+                value++;
+            }
+        }
+
+        // Výpis matice
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println(); // Nový řádek po každém řádku matice
+        }
+
+
 
 
     }
--- src/ulohy/Polia.java
+++ src/ulohy/Polia.java
@@ -4,21 +4,31 @@ public class Polia {
     public static void main(String[] args) {
         // Deklariacia pola s nazvom cars ktore obshauje  4 znacky
         String cars[]  = {"Volvo", "BMW", "Ford", "Mazda"};
+
         // vypis 1 znacku
         System.out.println(cars[0]);
+
         // vypis poseldnu
         System.out.println(cars[3]);
+
         //zmena znacky 1 auta
         cars[0] = "Opel";
+
         //a vypis tuto zmenu
         System.out.println(cars[0]);
+
         // pocet vsetky znaciek aut a poli je
         System.out.println(cars.length);
+
         //vypis vsetky znacky aut v poli
         System.out.println("Vypisete značky aut: ");
         System.out.println("---------------");
-        // POuzitie cyklu s pevnym poctom opakovanim a prikatu lengh ktory zistuje dlzku pola
 
+        // Pouzijeme cyklus s pevnym poctom opakovani a prikazu length, ktory zistuje dlzku pola
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println(cars[i]);
+            // mozeme v cykle vykonat aj viacero prikazov
+        }
 
     }
 }
