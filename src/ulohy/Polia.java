0729b3eb20e7f12aae43372075b31d0967a9a5f3
pavere <kostrab@icloud.com>
2023-08-28 18:22:43 +0200

    Polia

---
+++ src/ulohy/Polia.java
@@ -0,0 +1,12 @@
+package ulohy;
+
+public class Polia {
+    public static void main(String[] args) {
+        // Deklariacia pola s nazvom cars ktore obshauje  4 znacky
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        // vypis 1 znacku
+        System.out.println(cars[0]);
+        System.out.println(cars[2]);
+
+    }
+}
