4897ac0e2339b6954ed905cfaaa1378114dadff8
pavere <kostrab@icloud.com>
2023-08-28 20:52:50 +0200

    excel

--- src/ulohy/ZakladneExcelFunkcie.java
+++ src/ulohy/ZakladneExcelFunkcie.java
@@ -1,17 +1,21 @@
 package ulohy;
-
+/*
+Úloha č. 1: Úloha na základné Excel funkcie.
+        Vytvorte pole s ľubovoľným počtom čísel (napr. 5-tich), ktorými budeme prechádzať a jednotlivé hodnoty sčítavať, čiže výsledkom bude celkový súčet hodnôt.
+        */
 public class ZakladneExcelFunkcie {
-    public static void main(String[] args) {
-        int[] numbers = {5, 7, 15, 28, 35}; // Pole  cisla
 
-        int sum = 0;
-
-        // Scitanie v poli
-        for (int num : numbers) {
-            sum += num;
+    public static double scitaj(double cisla[]) {
+        double sum = 0;
+        for (int i = 0; i < cisla.length; i++) {
+            //sum = sum + cisla[i];
+            sum += cisla[i]; // Identicky vyznam ako riadok vyssie
         }
+        return sum;
+    }
 
-        System.out.println("Celkový súčet hodnot v poli je: " + sum);
-
+    public static void main(String[] args) {
+        double [] cisla = {10.5, 20, 30, 40, 50}; // Vytvorenie a naplnenie pola s desatinnymi cislami
+        System.out.println("Výsledok sčítania hodnôt v poli je: " + scitaj(cisla));
     }
 }
