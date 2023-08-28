8ba8c5295b5b680b8a6896dae4eeca292bfd8e28
pavere <kostrab@icloud.com>
2023-08-28 17:55:00 +0200

    priradenie

--- src/ulohy/PriradenieZnamkyTextovemuHodnoteniu.java
+++ src/ulohy/PriradenieZnamkyTextovemuHodnoteniu.java
@@ -6,49 +6,45 @@ Popis programu.
         Program bude ďalej pokračovať vypísaním otázky na zadanie slovnej známky, kým používateľ nepreruší program zadaním slova: koniec
         Program po zadaní koniec ukončí svoju činnosť a nevypíše nič.
 */
+
 import java.text.Normalizer;
 import java.util.Scanner;
 
 public class PriradenieZnamkyTextovemuHodnoteniu {
 
-    public static String stripAccents(String s)
-    {
+    public static String stripAccents(String s) {
         s = Normalizer.normalize(s, Normalizer.Form.NFD);
         s = s.replaceAll("[\\p{InCombiningDiacriticalMarks}]", "");
         return s;
     }
 
-
     public static void main(String[] args) {
 
+        String vstupnaHodnota = "";
+        String vstupnaTextovaZnamkaBezDiakritikyMalePismena = "";
+
         Scanner sc = new Scanner(System.in); // Inicializacia skeneru pre pouzivatelsky vstup
+        while (!vstupnaTextovaZnamkaBezDiakritikyMalePismena.equals("koniec")) {
+            System.out.println("Prosím zadaj textovú známku (výborný, chválitebný, dobrý, dostatočný, nedostatočný): ");
 
-        System.out.println("Prosím zadaj textovú známku (výborný, chválitebný, dobrý, dostatočný, nedostatočný): ");
-        String vstupnaHodnota = sc.nextLine(); // Priamy pouzivatelsky vstup, vidiet v konzole. Na nacitanie String musime pouzit nextLine
+            vstupnaHodnota = sc.nextLine(); // Priamy pouzivatelsky vstup, vidiet v konzole. Na nacitanie String musime pouzit nextLine
+            vstupnaTextovaZnamkaBezDiakritikyMalePismena = stripAccents(vstupnaHodnota).toLowerCase().trim(); // Odstranenie diakritiky a konverzia textu na male pismena a zaroven odstranenie medzier na zaciatku a konci
 
-        String vstupnaTextovaZnamka = stripAccents(vstupnaHodnota).toLowerCase().trim(); // Odstranenie diakritiky a konverzia textu na male pismena a zaroven odstranenie medzier na zaciatku a konci
-
-        //System.out.println(vstupnaTextovaZnamka);
-
-        if (!vstupnaTextovaZnamka.equals("vyborny")) // Ukazka negacie equals, vykricnik je na zaciatku premennej
-            System.out.println("Nezadal si známku výborný");
-
-
-        switch (vstupnaTextovaZnamka) {
-            case ("vyborny") -> {
+            if (vstupnaTextovaZnamkaBezDiakritikyMalePismena.equals("vyborny")) {
                 System.out.println(1);
-                System.out.println("Si najlepší TT");
-            }
-
-            case ("chvalitebny") ->
-                //System.out.println(2);
-                    System.out.println(2);
-            case ("dobry") -> System.out.println(3);
-            case ("dostatocny") -> System.out.println(4);
-            case ("nedostatocny") -> System.out.println(5);
-            default -> System.out.println("Neznámy vstup!");
+                System.out.println("Si najlepší");
+            } else if (vstupnaTextovaZnamkaBezDiakritikyMalePismena.equals("chvalitebny"))
+                System.out.println(2);
+            else if (vstupnaTextovaZnamkaBezDiakritikyMalePismena.equals("dobry"))
+                System.out.println(3);
+            else if (vstupnaTextovaZnamkaBezDiakritikyMalePismena.equals("dobry"))
+                System.out.println(4);
+            else if (vstupnaTextovaZnamkaBezDiakritikyMalePismena.equals("dobry"))
+                System.out.println(5);
+            else if (vstupnaTextovaZnamkaBezDiakritikyMalePismena.equals("koniec"))
+                System.out.println("Ukončenie programu");
+            else
+                System.out.println("Neznámy vstup!");
         }
-
-
     }
 }
