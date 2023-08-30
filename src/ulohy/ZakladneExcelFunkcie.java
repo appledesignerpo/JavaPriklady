a48b676904fb1c8d295f4f1cd5cf7826d976829a
pavere <kostrab@icloud.com>
2023-08-30 19:00:54 +0200

    Zakladen funkcie excel TODO DOPLNENE

--- src/ulohy/ZakladneExcelFunkcie.java
+++ src/ulohy/ZakladneExcelFunkcie.java
@@ -16,6 +16,18 @@ public class ZakladneExcelFunkcie {
 
         double priemerEfektivne = priemerujEfektivne(cisla);
         System.out.println("Výsledok EFEKTIVNÉHO priemerovania hodnôt v poli je: " + priemerEfektivne);
+
+        // vytvor metodu s nazvom pocet na ziskanie poctu vstupnych hodnot pola*
+        int pocetHodnot = pocet(cisla);
+        System.out.println("Počet hodnôt v poli je: " + pocetHodnot);
+
+        //Vytvor metodu s nazvom max na ziskanie najvyssej hodnoty **
+        double maxHodnota = max(cisla);
+        System.out.println("Najväčšia hodnota v poli je: " + maxHodnota);
+
+        // Vytvor metodu s nazvom min na ziskanie najnizsej hodnoty ***
+        double minHodnota = min(cisla);
+        System.out.println("Najnižšia hodnota v poli je: " + minHodnota);
     }
 
     // Metoda na scitanie hodnot v poli
@@ -42,5 +54,39 @@ public class ZakladneExcelFunkcie {
     private static double priemerujEfektivne(double[] cisla) {
         return scitaj(cisla) / cisla.length;
     }
+
+    //vytvor metodu s nazvom pocet na ziskanie poctu vstupnych hodnot pola*
+    private static int pocet(double[] cisla) {
+        return cisla.length;
+    }
+
+
+    //Vytvor metodu s nazvom max na ziskanie najvyssej hodnoty **
+    private static double max(double[] cisla) {
+        double max = cisla[0]; // Predpokladáme, že prvý prvok je zároveň najväčší
+        for (int i = 1; i < cisla.length; i++) {
+            if (cisla[i] > max) {
+                max = cisla[i]; // Aktualizácia najväčšej hodnoty, ak je nájdený väčší prvok
+            }
+        }
+        return max;
+    }
+
+   // Vytvor metodu s nazvom min na ziskanie najnizsej hodnoty ***
+    private static double min(double[] cisla) {
+        double min = cisla[0]; // Predpokladáme, že prvý prvok je zároveň najnižší
+        for (int i = 1; i < cisla.length; i++) {
+            if (cisla[i] < min) {
+                min = cisla[i]; // Aktualizácia najnižšej hodnoty, ak je nájdený menší prvok
+            }
+        }
+        return min;
+    }
+
+
+    // TODO Vytvor metodu s nazvom pocet na zistenie poctu vstupnych hodnot (pola)*
+    // TODO Vytvor metodu s nazvom max na ziskanie najvyssej hodnoty **
+    // TODO Vytvor metodu s nazvom min na ziskanie najnizsej hodnoty ***
+
 
 }
