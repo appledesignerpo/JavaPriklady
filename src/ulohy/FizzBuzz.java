65ce2f32d00f2e603fd95dc44b0ae48526c1d6bb
pavere <kostrab@icloud.com>
2023-08-30 20:09:18 +0200

    fizzbuz

--- src/ulohy/FizzBuzz.java
+++ src/ulohy/FizzBuzz.java
@@ -2,17 +2,19 @@ package ulohy;
 
 public class FizzBuzz {
     public static void main(String[] args) {
-        int rozsah = 100; // rozsah
+        int rozsah = 100; // Rozsah
 
+        // Cyklus prechádzania cez čísla od 1 do zadaného rozsahu
         for (int i = 1; i <= rozsah; i++) {
+            // Podmienka pre FizzBuzz
             if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FizzBuzz");
+                System.out.println("FizzBuzz"); // Ak je číslo deliteľné 3 a 5
             } else if (i % 3 == 0) {
-                System.out.println("Fizz");
+                System.out.println("Fizz"); // Ak je číslo deliteľné 3
             } else if (i % 5 == 0) {
-                System.out.println("Buzz");
+                System.out.println("Buzz"); // Ak je číslo deliteľné 5
             } else {
-                System.out.println(i);
+                System.out.println(i); // Inak, vypíšeme samotné číslo
             }
         }
     }
